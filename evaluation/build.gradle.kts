a62176c18fbe8a74dcd4b4298973fb91f566bfa2
Charlie Sale <softwaresale01@gmail.com>
2024-06-28 13:41:14 -0400

    Try again with a view

--- evaluation/build.gradle.kts
+++ evaluation/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "edu.purdue.dualitylab"
-version = "0.2.3"
+version = "0.2.4"
 
 repositories {
     mavenCentral()
--- evaluation/src/main/resources/sql/load_test_suites.sql
+++ evaluation/src/main/resources/sql/load_test_suites.sql
@@ -1,16 +1,4 @@
 
-WITH candidate_regexes_and_origins AS (
-    SELECT regex_entity.id AS regex_id, project_spec.id AS project_id, regex_source_usage.id AS source_usage_id
-    FROM regex_source_usage
-             -- take regexes that are static and dynamic and have source usages
-             INNER JOIN regex_entity ON regex_source_usage.regex_id = regex_entity.id
-        -- also pull project info
-             INNER JOIN project_spec ON regex_source_usage.project_id = project_spec.id
-    WHERE
-        regex_entity.static = TRUE AND
-        regex_entity.dynamic = TRUE AND
-        FILTER_REGEX_PATTERN(regex_entity.pattern, 5)
-)
 -- pull the regex pattern, originating project, source usage, example string, and regex function
 SELECT
     regex_entity.id AS regex_id,
