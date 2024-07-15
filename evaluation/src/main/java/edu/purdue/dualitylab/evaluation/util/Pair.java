16b7bda34a328869dba4326e77c8a33a17dfb635
Charlie Sale <softwaresale01@gmail.com>
2024-07-15 12:00:14 -0400

    Automated tests for AstDistance

---
+++ evaluation/src/main/java/edu/purdue/dualitylab/evaluation/util/Pair.java
@@ -0,0 +1,10 @@
+package edu.purdue.dualitylab.evaluation.util;
+
+public record Pair<A, B>(
+        A a,
+        B b
+) {
+    public static <A, B> Pair<A, B> of(A a, B b) {
+        return new Pair<>(a, b);
+    }
+}
