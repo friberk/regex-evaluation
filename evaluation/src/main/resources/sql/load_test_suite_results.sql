0cd03fbb127bd17ff254c39a1d933958df22e65b
Charlie Sale <softwaresale01@gmail.com>
2024-07-22 14:06:51 -0400

    Order test_suite_result load query by regex id for better cache performance
    I order results by (truth, candidate) regex pairs to improve cache
    performance. If we see an item four times in a row, then it'll be cached
    for the remaining three.

--- evaluation/src/main/resources/sql/load_test_suite_results.sql
+++ evaluation/src/main/resources/sql/load_test_suite_results.sql
@@ -9,3 +9,7 @@ FROM test_suite
 INNER JOIN test_suite_result ON test_suite.id = test_suite_result.test_suite_id
 INNER JOIN regex_entity AS truth_regex ON truth_regex.id = test_suite.regex_id
 INNER JOIN regex_entity AS candidate_regex ON candidate_regex.id = test_suite_result.regex_id
+-- add this order by to get better cache performance.
+ORDER BY
+    truth_regex_id,
+    candidate_regex_id;
