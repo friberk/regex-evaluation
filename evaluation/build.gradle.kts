a44fccb0322323b325635fe152032d77b3ba3fd3
Charlie Sale <softwaresale01@gmail.com>
2024-08-01 11:48:34 -0400

    Update jackson version

--- evaluation/build.gradle.kts
+++ evaluation/build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
     implementation(project(":dk.brics.automaton"))
 
     // serialization
-    implementation(platform("com.fasterxml.jackson:jackson-bom:2.13.4"))
+    implementation(platform("com.fasterxml.jackson:jackson-bom:2.17.2"))
     implementation("com.fasterxml.jackson.core:jackson-core")
     implementation("com.fasterxml.jackson.core:jackson-databind")
     implementation("com.fasterxml.jackson.core:jackson-annotations")
